00004b74d393b804f8506097e46953d55da91227
danieljlt <58784523+danieljlt@users.noreply.github.com>
2023-11-19 13:52:52 -0500

    router setup

---
+++ ui/src/Generation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  Button, MantineProvider} from '@mantine/core';
+
+function Generation() {
+    return (
+        <MantineProvider>
+        <Button>
+            Generate (im feel hungry yyummty)
+        </Button>
+        </MantineProvider>
+    );
+}
+
+export default Generation
+
--- ui/src/main.jsx
+++ ui/src/main.jsx
@@ -6,12 +6,22 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
+import Preferences from './Preferences.jsx';
+import Generation from './Generation.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App/>,
   },
+  {
+    path: "/preferences",
+    element: <Preferences/>
+  },
+  {
+    path: "/generation",
+    element: <Generation/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(

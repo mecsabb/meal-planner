fdc5848737db395b38c7f41db4c0f523df09eaa5
danieljlt <58784523+danieljlt@users.noreply.github.com>
2023-11-19 14:50:20 -0500

    spaghetti!

--- ui/src/Preferences.jsx
+++ ui/src/Preferences.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import {  Button, MantineProvider} from '@mantine/core';
+import {  Button, Card, MantineProvider, Group, Stack, Image} from '@mantine/core';
+import logo from './assets/sddefault.jpg';
+
+function yesEvent() {
+    
+}
+
+function noEvent() {
+
+}
+
+const meals = {
+    
+}
 
 function Preferences() {
     return (
         <MantineProvider>
-        <Button>
-            Preferences (i feel extra hungry)
-        </Button>
+            <Stack>
+                <Image src={logo}>
+
+                </Image>
+                <Group justify='center' gap={'xl'} grow>
+                    <Button color='green' variant='light' size='xl' onClick={yesEvent}>
+                        Yes
+                    </Button>
+                    <Button color='red' variant='light' size='xl' onClick={noEvent}>
+                        No
+                    </Button>
+                </Group>
+            </Stack>
         </MantineProvider>
     );
 }
